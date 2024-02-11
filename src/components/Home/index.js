ed5205d7039a7ad23046781fde20c61afe7aac87
Chandrasekhar Kuruva <148301206+chandrasekhar2145@users.noreply.github.com>
2024-02-11 17:29:38 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 // Write your JS code here
 import './index.css'
 
-const Home = () => {
+const Home = () => (
   ;<div className="home-container">
     <img
       src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
@@ -10,6 +10,6 @@ const Home = () => {
     />
     <h1 className="home-heading">Home</h1>
   </div>
-}
+)
 
 export default Home
